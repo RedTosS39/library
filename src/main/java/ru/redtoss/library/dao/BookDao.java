83b1d12f5b11d8dc0b5f18f4ab85f6bdb5fef1c2
redtoss39 <redtoss@gmail.com>
2025-08-18 09:04:40 +0200

    +addBook, +addPeople, +deletePeople, +updatePeople

--- src/main/java/ru/redtoss/library/dao/BookDao.java
+++ src/main/java/ru/redtoss/library/dao/BookDao.java
@@ -1,14 +1,11 @@
 package ru.redtoss.library.dao;
 
-import org.jspecify.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.redtoss.library.models.Book;
-import ru.redtoss.library.models.Person;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -21,8 +18,25 @@ public class BookDao {
         this.jdbcTemplate = jdbcTemplate;
     }
 
-    public List<Book> getBook() {
-        String SQL = "SELECT * FROM book";
-        return jdbcTemplate.query(SQL, new BeanPropertyRowMapper<>(Book.class));
+    public List<Book> getBooks() {
+        return jdbcTemplate.query("SELECT * FROM book", new BeanPropertyRowMapper<>(Book.class));
+    }
+
+    public Book getBook(int id) {
+        return jdbcTemplate.query("Select * from book where book_id = ?",
+                new Object[]{id},
+                new BeanPropertyRowMapper<>(Book.class)).stream().findAny().orElse(null);
+    }
+
+    public void editBook(Book book) {
+        jdbcTemplate.update("UPDATE book SET title=?, author=?, year=? WHERE book_id=?",
+                book.getTitle(), book.getAuthor(), book.getYear(), book.getBook_id());
+    }
+
+
+    public void addBook() {
+        Book book = new Book();
+        jdbcTemplate.update("INSERT INTO book (title, author, year) VALUES (?, ?, ?)",
+                book.getTitle(), book.getAuthor(), book.getYear());
     }
 }
